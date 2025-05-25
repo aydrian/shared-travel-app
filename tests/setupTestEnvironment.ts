5da23053857c176cb8839976fe74d0aa3e2974e8
Aydrian Howard <aydrian@gmail.com>
2025-05-25 13:18:55 -0400

    chore: code cleanup

--- tests/setupTestEnvironment.ts
+++
@@ -1,9 +0,0 @@
-import { setupTestData } from './testSetup';
-
-export async function setup() {
-  console.log('Setting up test environment...');
-  await setupTestData();
-  console.log('Test environment setup complete.');
-}
-
-setup().catch(console.error);
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import {
   readD1Migrations
 } from "@cloudflare/vitest-pool-workers/config";
 import path from "node:path";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineWorkersConfig(async () => {
   // Read all migrations in the `migrations` directory
@@ -38,9 +37,6 @@ export default defineWorkersConfig(async () => {
           miniflare: {
             // Add a test-only binding for migrations, so we can apply them in a
             // setup file
-            compatibilityFlags: ["nodejs_compat"],
-            compatibilityDate: "2025-04-24",
-            d1Databases: ["DB"],
             bindings: { TEST_MIGRATIONS: migrations, NODE_ENV: "test" }
           }
         }
@@ -50,10 +46,6 @@ export default defineWorkersConfig(async () => {
       alias: {
         "@": path.resolve(__dirname, "./src")
       }
-    },
-    plugins: [nodePolyfills({ include: ["https", "http", "url"] })],
-    define: {
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }
   };
 });
